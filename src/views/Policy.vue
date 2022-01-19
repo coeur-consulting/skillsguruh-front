a0c2d7512fe736efbbd86e0bcbc3c5694f8f8bb2
arudovwen <successahon@gmail.com>
2022-01-19 15:51:21 +0100

    Update Policy.vue

--- src/views/Policy.vue
+++ src/views/Policy.vue
@@ -1,16 +1,358 @@
 <template>
- <section class="mt-5">
+  <section class="">
+    <div class="bg-dark" style="height:80px">
+
+    </div>
     <b-container>
-    <b-row>
-      <b-col  class="p-5">
-        Policy
-      </b-col>
-    </b-row>
-  </b-container>
- </section>
+      <b-row>
+        <b-col class="p-5 text-left">
+          <h1>NZUKOOR PRIVACY POLICY</h1>
+          <p>
+            Your privacy is important to Nzukoor and always has been. So we've
+            developed a Privacy Policy that covers how we collect, use,
+            disclose, transfer, and store your information. Please take a moment
+            to familiarize yourself with our privacy practices and let us know
+            if you have any questions. By visiting Nzukoor, you are accepting
+            the practices described in this Privacy Notice.
+          </p>
+
+          <h5>OVERVIEW</h5>
+          <ol>
+            <li>What kinds of information do we collect?</li>
+            <li>How do we use this information?</li>
+            <li>How is this information shared?</li>
+            <li>How do we protect your personal data?</li>
+            <li>What are your rights?</li>
+            <li>How can I manage or delete information about me?</li>
+            <li>Law And Protection</li>
+            <li>How will we notify you of changes to this Policy?</li>
+          </ol>
+          <h5>What kinds of information do we collect?</h5>
+          <p>
+            To provide the Nzukoor Services, we must process information about
+            you. The type of information that we collect depends on how you use
+            our Services.
+          </p>
+          <h6>Things that you and others do and provide:</h6>
+          <ul>
+            <li>
+              <span class="font-weight-bold"
+                >Information and content you provide.</span
+              >
+              We collect the content, communications and other information you
+              provide when you use our Services, including when you sign up for
+              an account, create or share content and message or communicate
+              with others.
+            </li>
+            <li>
+              <span class="font-weight-bold">Networks and connections.</span> We
+              collect information about the people, pages accounts, hashtags and
+              groups that you are connected to and how you interact with them
+              across our Products, such as people you communicate with the most
+              or groups that you are part of.
+            </li>
+            <li>
+              <span class="font-weight-bold">Your usage.</span> We collect
+              information about how you use our Services, such as the
+              interactive forums, types of content that you view or engage with,
+              the features you use, the actions you take, the people or accounts
+              you interact with and the time, frequency and duration of your
+              activities. For example, we log when you're using and have last
+              used our Products, and what posts, videos and other content you
+              view on our Products.
+            </li>
+          </ul>
+
+          <h6>How do we use this information?</h6>
+          <p>
+            We use the information we have to deliver our Services, including to
+            personalise features and make suggestions for you (such as groups or
+            events you may be interested in or topics you may want to follow) on
+            and off our Products. To create personalised Services that are
+            unique and relevant to you, we use your connections, preferences,
+            interests and activities based on the data that we collect and learn
+            from you and others (including any data with special protection you
+            choose to provide); how you use and interact with our Products; and
+            the people, places or things that you're connected to and interested
+            in on and off our Products.
+          </p>
+          <h6>Product research and development:</h6>
+          <p>
+            We use the information we have to develop, test and improve our
+            Services, including by conducting surveys and research, and testing
+            and troubleshooting new products and features.
+          </p>
+          <h6>Communicate with you.</h6>
+          <p>
+            We use the information that we have to communicate with you about
+            our Services and let you know about our Policies and Terms. We also
+            use your information to respond to you when you contact us. Research
+            and innovate for social good.
+          </p>
+
+          <p>
+            We use the information that we have (including from research
+            partners who we collaborate with) to conduct and support research
+            and innovation on digital communication, particularly tailored to
+            the African market and technological advancement for digital social
+            networking within the region.
+          </p>
+
+          <h6>How is this information shared?</h6>
+          <p>Your information is shared with others in the following ways:</p>
+          <h6>People and accounts that you share and communicate with:</h6>
+          <p>
+            When, you use Nzukoor to communicate with people or businesses,
+            those people and businesses can see the content you send. Your
+            network can also see actions that you have taken on our Products,
+            including engagement with ads and sponsored content. We also let
+            other accounts see who has viewed Nzukoor Stories.
+          </p>
+          <p>
+            Public information can be seen by anyone, on or off our Services,
+            including if they don't have an account. This includes your Nzukoor
+            username, any information you share with a public audience,
+            information in your public profile on Nzukoor, and content you share
+            on any other public forum, such as Nzukoor Marketplace. You, other
+            people using Nzukoor and we can provide access to or send public
+            information to anyone on or off our Services, in search results or
+            through tools and APIs. Public information can also be seen,
+            accessed, reshared or downloaded through third-party services such
+            as search engines, APIs and offline media such as TV, and by apps,
+            websites and other services that integrate with our Products.
+          </p>
+
+          <h6>
+            Information about your active status or presence on our Platform:
+          </h6>
+          <p>
+            People in your networks can see signals telling them whether you are
+            active on our platform, or when you last used our Services.
+          </p>
+          <h6>
+            Apps, websites and third-party integrations on or using our
+            Services.
+          </h6>
+          <p>
+            When you choose to use third-party apps, websites or other services
+            that use, or are integrated with, our Products, they can receive
+            information about what you post or share. Information collected by
+            these third-party services is subject to their own terms and
+            policies, not this one.
+          </p>
+          <h6>Sharing with third-party partners</h6>
+          <p>
+            We work with third-party partners who help us provide and improve
+            our Services or who use Nzukoor to grow their businesses, which
+            makes it possible to operate our companies and provide free services
+            to people around the world. We don't sell any of your information to
+            anyone and we never will. We also impose strict restrictions on how
+            our partners can use and disclose the data we provide. Here are the
+            types of third parties that we share information with:
+          </p>
+          <h6>Partners who use our analytics services.</h6>
+          <p>
+            We provide aggregated statistics and insights that help people and
+            businesses understand how people are engaging with their posts,
+            listings, Pages, videos and other content on and off the Nzukoor
+            Platform. For example, Page admins and Nzukoor business profiles
+            receive information about the number of people or accounts who
+            viewed, reacted to or commented on their posts, as well as aggregate
+            demographic and other information that helps them understand
+            interactions with their Page or account.
+          </p>
+          <h6>Advertisers.</h6>
+          <p>
+            We provide advertisers with reports about the kinds of people seeing
+            their ads and how their ads are performing, but we don't share
+            information that personally identifies you (information such as your
+            name or email address that by itself can be used to contact you or
+            identifies who you are) unless you give us permission. For example,
+            we provide general demographic and interest information to
+            advertisers (for example, that an ad was seen by a woman between the
+            ages of 25 and 34 who lives in Ghana and likes software engineering)
+            to help them better understand their audience. We also confirm which
+            Nzukoor ads led you to make a purchase or take an action with an
+            advertiser.
+          </p>
+          <h6>Device and Connection Information.</h6>
+          <p>
+            We collect device-specific information when you install, access, or
+            use our Services. This includes information such as hardware model,
+            operating system information, browser information, IP address,
+            mobile network information including phone number, and device
+            identifiers. We collect device location information if you use our
+            location features, such as when you choose to share your location
+            with your contacts, view locations nearby or those others have
+            shared with you, and the like, and for diagnostics and
+            troubleshooting purposes such as if you are having trouble with our
+            app’s location features.
+          </p>
+          <h6>Researchers and academics.</h6>
+          <p>
+            We also provide information and content to research partners and
+            academics to conduct research that advances scholarship and
+            innovation that supports our business mission of helping people find
+            fulfillment in their day-to-day lives by connecting them to people,
+            knowledge and opportunities.
+          </p>
+          <h6>Law enforcement or legal requests.</h6>
+          <p>
+            We share information with law enforcement or in response to legal
+            requests
+          </p>
+          <h6>How do we protect your personal data?</h6>
+          <p>
+            Nzukoor takes the security of your personal data very seriously. We
+            make every effort to protect your personal data from misuse,
+            interference, loss, unauthorised access, modification or disclosure.
+          </p>
+          <p>
+            Our measures include implementing appropriate access controls,
+            investing in the latest Information Security Capabilities to protect
+            the IT environments we leverage, and ensuring we encrypt,
+            pseudonymise and anonymise personal data wherever possible.
+          </p>
+          <p>
+            Access to your personal data is only permitted among our employees
+            and agents on a need-to-know basis and subject to strict contractual
+            confidentiality obligations when processed by third-parties.
+          </p>
+          <h6>What are your rights?</h6>
+          <p>
+            Your rights in relation to your personal data, how it is processed.
+          </p>
+          <p>
+            Where we process your personal data, you have a number of rights
+            over how the data is processed and can exercise these rights at any
+            point. We have provided an overview of these rights below together
+            with what this entails for you. You can exercise your rights by
+            sending an email or submitting a request through the “Contact Us”
+            form on our websites.
+          </p>
+          <ul>
+            <li>
+              The right to be informed. You have the right to be provided with
+              clear, transparent and easily understandable information about how
+              we use your personal data and your rights. Therefore, we’re
+              providing you with the information in this Notice.
+            </li>
+            <li>
+              The right to access and rectification. You have the right to
+              access, correct or update your personal data at any time. We
+              understand the importance of this and should you want to exercise
+              your rights, please contact us.
+            </li>
+            <li>
+              The right to data portability. The personal data you have provided
+              us with is portable. This means it can be moved, copied or
+              transmitted electronically under certain circumstances.
+            </li>
+            <li>
+              The right to be forgotten. Under certain circumstances, you have
+              right to request that we delete your data. If you wish to delete
+              the personal data we hold about you, please let us know and we
+              will take reasonable steps to respond to your request in
+              accordance with legal requirements. If the personal data we
+              collect is no longer needed for any purposes and we are not
+              required by law to retain it, we will do what we can to delete,
+              destroy or permanently de-identify it.
+            </li>
+            <li>
+              The right to object. Under certain circumstances, you have the
+              right to object to certain types of processing, including
+              processing for direct marketing (i.e., receiving emails from us
+              notifying you or being contacted with varying potential
+              opportunities).
+            </li>
+            <li>
+              The right to lodge a complaint with a Supervisory Authority. You
+              have the right to lodge a complaint directly with any local
+              Supervisory Authority about how we process your personal data.
+            </li>
+            <li>
+              The right to withdraw consent. If you have given your consent to
+              anything we do with your personal data (i.e., we rely on consent
+              as a legal basis for processing your personal data), you have the
+              right to withdraw your consent at any time (although if you do so,
+              it does not mean that anything we have done with your personal
+              data with your consent up to that point is unlawful). You can
+              withdraw your consent to the processing of your personal data at
+              any time by contacting us with the details provided below.
+            </li>
+
+            <li>
+              Rights related to automated decision-making. You have the right
+              not to be subject to a decision which is based solely on automated
+              processing and which produces legal or other significant effects
+              on you. In particular, you have the right:
+              <ul>
+                <li>to obtain human intervention;</li>
+                <li>to express your point of view;</li>
+                <li>
+                  to obtain an explanation of the decision reached after an
+                  assessment; and
+                </li>
+                <li>to challenge such a decision.</li>
+              </ul>
+            </li>
+          </ul>
+
+          <h6>How can I manage or delete information about me?</h6>
+          <p>
+            We provide you with the ability to access, rectify, port and delete
+            your data.
+          </p>
+          <p>
+            We store data until it is no longer necessary to provide our
+            services or until your account is deleted – whichever comes first.
+            This is a case-by-case determination that depends on things such as
+            the nature of the data, why it is collected and processed, and
+            relevant legal or operational retention needs. For example, when you
+            search for something on Nzukoor, you can access and delete that
+            query from within your search history at any time, but the log of
+            that search is deleted after six months. If you submit a copy of
+            your valid photo ID for account verification purposes, we delete
+            that copy 30 days after submission.
+          </p>
+          <p>
+            When you delete your account, we delete things that you have posted,
+            and you won't be able to recover this information later. Information
+            that others have shared about you isn't part of your account and
+            won't be deleted. If you don't want to delete your account but want
+            to temporarily stop using the Products, you can deactivate your
+            account instead. To delete your account at any time, please visit
+            your Nzukoor profile.
+          </p>
+          <h6>Law And Protection</h6>
+          <p>
+            We may collect, use, preserve, and share your information if we have
+            a good-faith belief that it is reasonably necessary to: (a) respond
+            pursuant to applicable law or regulations, to legal process, or to
+            government requests; (b) enforce our Terms and any other applicable
+            terms and policies, including for investigations of potential
+            violations; (c) detect, investigate, prevent, and address fraud and
+            other illegal activity, security, or technical issues; or (d)
+            protect the rights, property, and safety of our users, or others.
+          </p>
+          <h6>How will we notify you of changes to this Policy?</h6>
+          <p>
+            We'll notify you before we make changes to this Policy and give you
+            the opportunity to review the revised Policy before you choose to
+            continue using our Products.
+          </p>
+          <h5>Contact Us</h5>
+          <p>
+            If you have questions about our Privacy Policy, please contact us;
+          </p>
+          <p>Support@nzukoor.com</p>
+        </b-col>
+      </b-row>
+    </b-container>
+  </section>
 </template>
 <style scoped>
-section{
+section {
   min-height: 80vh;
 }
 </style>
